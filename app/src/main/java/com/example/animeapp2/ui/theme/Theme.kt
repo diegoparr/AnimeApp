90f78f74b4420ad645bce2fe66b991fcf1c4964f
diegoparr <diego.parrado3322@gmail.com>
2026-03-31 11:17:24 -0500

    theme and fonts usage added

--- app/src/main/java/com/example/animeapp2/ui/theme/Theme.kt
+++ app/src/main/java/com/example/animeapp2/ui/theme/Theme.kt
@@ -51,7 +51,7 @@ private val LightColorScheme = lightColorScheme(
 fun AnimeApp2Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
