6620dfee8c723b6f9a6f3049a240ac938187a3b2
diegoparr <diego.parrado3322@gmail.com>
2026-04-01 17:08:30 -0500

    add ML google translate dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation("io.coil-kt.coil3:coil-compose:3.4.0")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.4.0")
     implementation("androidx.compose.ui:ui-text-google-fonts:1.7.5")
+    implementation(libs.ml.kit.translate)
     implementation(libs.androidx.compose.ui.text)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
