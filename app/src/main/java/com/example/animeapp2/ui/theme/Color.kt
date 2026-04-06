b8b02a404cf06d69273bc4e91e6a11d9ed5b065a
diegoparr <diego.parrado3322@gmail.com>
2026-04-05 21:01:21 -0500

    change and add theme colors

--- app/src/main/java/com/example/animeapp2/ui/theme/Color.kt
+++ app/src/main/java/com/example/animeapp2/ui/theme/Color.kt
@@ -5,7 +5,8 @@ import androidx.compose.ui.graphics.Color
 // Colores para el tema oscuro (The Eclipse)
 val DarkBackground = Color(0xFF0D0D0D) // Negro casi puro
 val DarkSurface = Color(0xFF1A1A1A)    // Gris muy oscuro para tarjetas
-val BloodRed = Color(0xFFE63946)       // Rojo vibrante para botones
+val BloodRed = Color(0xFF990000)// Rojo vibrante para botones
+val Accent = Color(0xFF4A0404)
 val GhostWhite = Color(0xFFF1FAEE)      // Blanco suave para que no canse la vista
 val DarkGray = Color(0xFF2B2B2B)       // Para bordes o detalles sutiles
 
