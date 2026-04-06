e078c7ce12d030f24f725df88668e8638945a2b3
diegoparr <diego.parrado3322@gmail.com>
2026-04-05 21:03:07 -0500

    implemented drawer button on app top bar

--- app/src/main/java/com/example/animeapp2/ui/components/CrimsonListTopBar.kt
+++ app/src/main/java/com/example/animeapp2/ui/components/CrimsonListTopBar.kt
@@ -5,11 +5,18 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,13 +28,19 @@ import com.example.animeapp2.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CrimsonListTopBar(){
+fun CrimsonListTopBar(drawerState: DrawerState){
 
     CenterAlignedTopAppBar(
+
+        navigationIcon = {
+            NavigationDrawerButton(drawerState)
+        },
+
         title = {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
             ) {
+
                 Image(
                     painter = painterResource(R.drawable.ic_app_icon),
                     contentDescription = null,
