552bc0699047c5c63e49eb217e378a4bd3efa6f0
diegoparr <diego.parrado3322@gmail.com>
2026-04-05 21:03:35 -0500

    add drawer menu component

---
+++ app/src/main/java/com/example/animeapp2/ui/components/DrawerMenu.kt
@@ -0,0 +1,53 @@
+package com.example.animeapp2.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun DrawerMenu(){
+
+    val drawerGradient = Brush.linearGradient(
+        colors = listOf(
+            Color(0xFF0A0A0A), // Negro profundo (Base)
+            Color(0xFF1A0505)  // Un rojo carmesí "sangre seca" casi negro (Acento)
+        ),
+        start = Offset(0f,0f),
+        end = Offset(0f, Float.POSITIVE_INFINITY)
+    )
+
+    ModalDrawerSheet(
+        drawerContainerColor = Color.Transparent,
+        drawerContentColor = MaterialTheme.colorScheme.onBackground,
+        modifier = Modifier
+            .fillMaxHeight()
+            .width(300.dp)
+            .background(drawerGradient)
+    ) {
+        Spacer(Modifier.height(24.dp))
+
+        Text(
+            text = "Crimson List",
+            style = MaterialTheme.typography.displayMedium.copy(),
+            letterSpacing = 1.5.sp,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+    }
+
+
+}
