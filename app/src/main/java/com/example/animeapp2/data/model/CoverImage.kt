adeb1c475f689a892a5287756a72ae3f8f27e045
diegoparr <diego.parrado3322@gmail.com>
2026-04-05 21:01:58 -0500

    add card color parameter on data class

--- app/src/main/java/com/example/animeapp2/data/model/CoverImage.kt
+++ app/src/main/java/com/example/animeapp2/data/model/CoverImage.kt
@@ -1,3 +1,4 @@
 package com.example.animeapp2.data.model
 
-data class CoverImage(val large : String)
+data class CoverImage(val large : String,
+                      val color : String? = "#2B2B2B")
