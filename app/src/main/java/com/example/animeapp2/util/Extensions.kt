54fbf654a56ae2eff5660d1e4b81ec8f3b73b2a8
diegoparr <diego.parrado3322@gmail.com>
2026-04-09 19:49:28 -0500

    update `cleanHtml` extension to use `HtmlCompat`

--- app/src/main/java/com/example/animeapp2/util/Extensions.kt
+++ app/src/main/java/com/example/animeapp2/util/Extensions.kt
@@ -1,6 +1,9 @@
 package com.example.animeapp2.util
+import androidx.core.text.HtmlCompat
 
+
+// Funcion para limpiar html que viene dentro del campo description de la API
 fun String.cleanHtml(): String {
-    return android.text.Html.fromHtml(this, android.text.Html.FROM_HTML_MODE_LEGACY).toString()
+    return HtmlCompat.fromHtml(this, HtmlCompat.FROM_HTML_MODE_LEGACY).toString()
 }
 
