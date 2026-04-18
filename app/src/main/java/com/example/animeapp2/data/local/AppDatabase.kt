6e314e51c3cbfc482269a5579246a77b48c488e3
diegoparr <diego.parrado3322@gmail.com>
2026-04-18 12:33:19 -0500

    create AppDatabase for local data storage using Room

---
+++ app/src/main/java/com/example/animeapp2/data/local/AppDatabase.kt
@@ -0,0 +1,46 @@
+package com.example.animeapp2.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.animeapp2.data.local.dao.AnimeMangaDao
+import com.example.animeapp2.data.local.dao.UserDao
+import com.example.animeapp2.data.local.entities.*
+
+@Database(
+    entities = [
+        UserEntity::class,
+        AnimeMangaEntity::class,
+        AnimeGenreCrossRef::class,
+        TranslationEntity::class,
+        AnimeMangaUserCrossRef::class,
+        GenreEntity::class
+    ],
+    version = 1
+
+
+
+)
+@TypeConverters(Converters::class)
+abstract class AppDatabase : RoomDatabase(){
+    abstract fun userDao() : UserDao
+    abstract fun animeMangaDao() : AnimeMangaDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: android.content.Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = androidx.room.Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "crimson_list_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
+
