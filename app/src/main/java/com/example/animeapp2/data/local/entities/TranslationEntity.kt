c90900dbe610199874835940ebccadbd5f67904b
diegoparr <diego.parrado3322@gmail.com>
2026-04-18 12:34:36 -0500

    add TranslationEntity for anime translations

---
+++ app/src/main/java/com/example/animeapp2/data/local/entities/TranslationEntity.kt
@@ -0,0 +1,23 @@
+package com.example.animeapp2.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+
+@Entity(
+    tableName = "anime_traducciones",
+    primaryKeys = ["id_animemanga","lenguaje"],
+    foreignKeys = [
+        ForeignKey(
+            entity = AnimeMangaEntity::class,
+            parentColumns = ["id_animemanga"],
+            childColumns = ["id_animemanga"],
+            onDelete = ForeignKey.CASCADE // Si se borra el anime, se borran sus traducciones
+        )
+    ]
+)
+data class TranslationEntity(
+    val id_animemanga: Int,
+    val lenguaje: String, // "es", "en", etc.
+    val descripcion_traducida: String,
+    val last_updated: Long = System.currentTimeMillis()
+)
