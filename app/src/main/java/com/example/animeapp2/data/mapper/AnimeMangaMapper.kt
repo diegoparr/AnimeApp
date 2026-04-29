07c84746de401eff2684b9a3c3d2da088a05cb12
diegoparr <diego.parrado3322@gmail.com>
2026-04-29 00:53:09 -0500

    update `AnimeMangaMapper` to use `MediaType` enum and add `toDomain` extension for `GetAnimeMangasListByQuery`

--- app/src/main/java/com/example/animeapp2/data/mapper/AnimeMangaMapper.kt
+++ app/src/main/java/com/example/animeapp2/data/mapper/AnimeMangaMapper.kt
@@ -1,9 +1,11 @@
 package com.example.animeapp2.data.mapper
 
+import com.example.animeapp2.GetAnimeMangasListByQuery
 import com.example.animeapp2.GetAnimeMangasListQuery
 import com.example.animeapp2.data.model.AnimeManga
 import com.example.animeapp2.data.model.Title
 import com.example.animeapp2.data.model.CoverImage
+import com.example.animeapp2.data.model.MediaType
 import com.example.animeapp2.util.cleanHtml
 
 // Este mapper transforma objetos que vengan de la API en la seccion Medium a objetos Kotlin de la
@@ -17,7 +19,36 @@ fun GetAnimeMangasListQuery.Medium.toDomain(): AnimeManga {
             english = this.title?.english ?: this.title?.romaji ?: "",
             native = this.title?.native ?: ""
         ),
-        type = this.type?.toString() ?: "UNKNOWN",
+        type = try{
+            MediaType.valueOf(this.type?.toString() ?: "DESCONOCIDO")
+        } catch(e: Exception){
+            MediaType.DESCONOCIDO
+        },
+        genres = this.genres?.filterNotNull() ?: emptyList(),
+        description = this.description?.cleanHtml() ?: "No description",
+        // 3. Mapeamos la imagen de portada que trae la api a un objeto CoverImage
+        coverImage = CoverImage(
+            large = this.coverImage?.large ?: "",
+            color = "#000000"
+        )
+    )
+}
+
+
+fun GetAnimeMangasListByQuery.Medium.toDomain() : AnimeManga{
+    return AnimeManga(
+        id = this.id,
+        // 2. Mapeamos el titulo que trae la api a un objeto Title
+        title = Title(
+            romaji = this.title?.romaji ?: "",
+            english = this.title?.english ?: this.title?.romaji ?: "",
+            native = this.title?.native ?: ""
+        ),
+        type = try{
+            MediaType.valueOf(this.type?.toString() ?: "DESCONOCIDO")
+        } catch(e: Exception){
+            MediaType.DESCONOCIDO
+        },
         genres = this.genres?.filterNotNull() ?: emptyList(),
         description = this.description?.cleanHtml() ?: "No description",
         // 3. Mapeamos la imagen de portada que trae la api a un objeto CoverImage
