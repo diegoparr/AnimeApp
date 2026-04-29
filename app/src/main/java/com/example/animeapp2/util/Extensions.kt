1910731b841ff9d8f537eac8975a7964f8613d16
diegoparr <diego.parrado3322@gmail.com>
2026-04-29 00:54:26 -0500

    add `isValidEmail` extension function to `String` in `Extensions.kt`

--- app/src/main/java/com/example/animeapp2/util/Extensions.kt
+++ app/src/main/java/com/example/animeapp2/util/Extensions.kt
@@ -7,3 +7,6 @@ fun String.cleanHtml(): String {
     return HtmlCompat.fromHtml(this, HtmlCompat.FROM_HTML_MODE_LEGACY).toString()
 }
 
+fun String.isValidEmail(): Boolean{
+    return android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
+}
