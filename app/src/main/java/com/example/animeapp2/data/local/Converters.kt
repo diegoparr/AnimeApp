ca4a278462b3a4bfece56237e9fc7930d46af5e9
diegoparr <diego.parrado3322@gmail.com>
2026-04-29 00:53:55 -0500

    update `Converters` to add `MediaType` support and default values for enum conversion

--- app/src/main/java/com/example/animeapp2/data/local/Converters.kt
+++ app/src/main/java/com/example/animeapp2/data/local/Converters.kt
@@ -2,6 +2,7 @@ package com.example.animeapp2.data.local
 
 import androidx.room.TypeConverter
 import com.example.animeapp2.data.model.AnimeStatus
+import com.example.animeapp2.data.model.MediaType
 
 class Converters{
 
@@ -9,7 +10,21 @@ class Converters{
     fun fromStatus(status : AnimeStatus): String = status.name
 
     @TypeConverter
-    fun toStatus(value: String): AnimeStatus = AnimeStatus.valueOf(value)
+    fun toStatus(value: String): AnimeStatus = try{
+        AnimeStatus.valueOf(value)
+    } catch (e: Exception){
+        AnimeStatus.PENDIENTE // Valor por defecto si algo falla
+    }
+
+    @TypeConverter
+    fun fromMediaType(type: MediaType): String = type.name
+
+    @TypeConverter
+    fun toMediaType(value: String): MediaType = try {
+        MediaType.valueOf(value)
+    } catch (e: Exception) {
+        MediaType.DESCONOCIDO
+    }
 
     @TypeConverter
     fun fromList(list: List<String>): String = list.joinToString(",")
