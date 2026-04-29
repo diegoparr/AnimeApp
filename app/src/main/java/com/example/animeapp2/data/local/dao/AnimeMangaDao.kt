38859049304ff28a6d52b618a9a0390f308ab236
diegoparr <diego.parrado3322@gmail.com>
2026-04-29 00:52:36 -0500

    add `insertToLibrary` method to `AnimeMangaDao`

--- app/src/main/java/com/example/animeapp2/data/local/dao/AnimeMangaDao.kt
+++ app/src/main/java/com/example/animeapp2/data/local/dao/AnimeMangaDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.animeapp2.data.local.entities.AnimeMangaEntity
+import com.example.animeapp2.data.local.entities.AnimeMangaUserCrossRef
 import com.example.animeapp2.data.local.entities.TranslationEntity
 
 @Dao
@@ -14,6 +15,10 @@ interface AnimeMangaDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTranslation(translation : TranslationEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertToLibrary(animeManga : AnimeMangaUserCrossRef)
+
 
     @Query("SELECT descripcion_traducida FROM anime_translations WHERE id_animemanga = :animeId AND lenguaje = :lang")
     suspend fun getTranslation(animeId : Int, lang: String = "es") : String?
