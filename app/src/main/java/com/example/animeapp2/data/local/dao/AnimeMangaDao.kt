952cf70dadb749d450ee4959173e95d7c4ac07bb
diegoparr <diego.parrado3322@gmail.com>
2026-05-02 19:49:03 -0500

    add `upsertToLibrary` and `getUserLibraryEntry` to `AnimeMangaDao`

--- app/src/main/java/com/example/animeapp2/data/local/dao/AnimeMangaDao.kt
+++ app/src/main/java/com/example/animeapp2/data/local/dao/AnimeMangaDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Upsert
 import com.example.animeapp2.data.local.entities.AnimeMangaEntity
 import com.example.animeapp2.data.local.entities.AnimeMangaUserCrossRef
 import com.example.animeapp2.data.local.entities.TranslationEntity
@@ -22,4 +23,10 @@ interface AnimeMangaDao {
 
     @Query("SELECT descripcion_traducida FROM anime_translations WHERE id_animemanga = :animeId AND lenguaje = :lang")
     suspend fun getTranslation(animeId : Int, lang: String = "es") : String?
+
+    @Upsert
+    suspend fun upsertToLibrary(crossRef: AnimeMangaUserCrossRef)
+
+    @Query("SELECT * FROM user_anime_library WHERE id_usuario = :userId AND id_animemanga = :animeId")
+    suspend fun getUserLibraryEntry(userId: Int, animeId: Int): AnimeMangaUserCrossRef?
 }
