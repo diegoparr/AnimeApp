8f2e203003e10146b1dd1cb92381f84b9dd5938f
diegoparr <diego.parrado3322@gmail.com>
2026-05-03 15:35:34 -0500

    add `getUserLibrary` and `removeFromLibrary` queries to `AnimeMangaDao`

--- app/src/main/java/com/example/animeapp2/data/local/dao/AnimeMangaDao.kt
+++ app/src/main/java/com/example/animeapp2/data/local/dao/AnimeMangaDao.kt
@@ -4,10 +4,13 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Upsert
 import com.example.animeapp2.data.local.entities.AnimeMangaEntity
 import com.example.animeapp2.data.local.entities.AnimeMangaUserCrossRef
 import com.example.animeapp2.data.local.entities.TranslationEntity
+import com.example.animeapp2.data.local.entities.UserLibraryItem
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface AnimeMangaDao {
@@ -29,4 +32,14 @@ interface AnimeMangaDao {
 
     @Query("SELECT * FROM user_anime_library WHERE id_usuario = :userId AND id_animemanga = :animeId")
     suspend fun getUserLibraryEntry(userId: Int, animeId: Int): AnimeMangaUserCrossRef?
+
+    @Transaction
+    @Query("SELECT * FROM user_anime_library WHERE id_usuario = :userId ORDER BY fecha_agregado DESC")
+    fun getUserLibrary(userId: Int): Flow<List<UserLibraryItem>>
+
+    @Query("DELETE FROM user_anime_library WHERE id_usuario = :userId AND id_animemanga = :animeId")
+    suspend fun removeFromLibrary(userId: Int, animeId: Int)
 }
+
+
+
